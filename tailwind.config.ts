ca379d6f9b2c9dd580b8418e6e8142d1e9b95a5e
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2024-08-06 21:30:02 +0900

    feat: change tailwind config
    - modify content
    - add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,40 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
+      colors: {
+        brand: {
+          orange: '#FF530F',
+          navy: '#1C224E',
+        },
+        gray: {
+          black: '#0B0D0F',
+          white: '#FFFFFF',
+          800: '#5B5C63',
+          600: '#B5B9C4',
+          500: '#CDD1DC',
+          400: '#E4E5E9',
+          300: '#F1F1F3',
+          200: '#F7F8FA',
+          100: '#F9FAFC',
+        },
+        text: {
+          600: '#0B0D0F',
+          500: '#5B5C63',
+          400: '#868A93',
+          300: '#B5B9C4',
+          200: '#CDD1DC',
+          100: '#FFFFFF',
+        },
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
