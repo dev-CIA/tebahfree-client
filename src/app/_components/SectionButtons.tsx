cfab3b6180d88fc8db5c0ec9681d9a375ec63207
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2024-08-11 22:36:00 +0900

    feat: SectionButtons component ui

---
+++ src/app/_components/SectionButtons.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { useState } from 'react'
+
+import { Button } from '@/components/ui/button'
+
+export const SectionButtons = () => {
+  const [selectedBtn, setSelectedBtn] = useState('organization')
+
+  return (
+    <div className="w-100vw flex flex-wrap gap-[0.3125rem] px-[0.5625rem]">
+      {[
+        { value: 'organization', label: '청년부 조직도' },
+        { value: 'worship', label: '청년부 예배' },
+        { value: 'group', label: '순모임' },
+        { value: 'new', label: '새가족순' },
+        { value: 'missionGroup', label: '선교 소모임' },
+        { value: 'unending', label: '언엔딩 워십' },
+        { value: 'info', label: '자주찾는 정보' },
+      ].map(({ value, label }, index) => (
+        <Button
+          key={`${value}-${index}`}
+          variant={selectedBtn === value ? 'default' : 'secondary'}
+          size={'chip'}>
+          {label}
+        </Button>
+      ))}
+    </div>
+  )
+}
