b3f77d647c2255eec863bb4c9110649f7476df3d
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2025-01-12 04:00:40 +0900

    feat: add 'third' variant to button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import * as React from 'react'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-B3_14_Regular ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-B3_14_Regular ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300',
   {
     variants: {
       variant: {
@@ -19,12 +19,14 @@ const buttonVariants = cva(
           'border border-g-100 bg-transparent dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50',
         secondary:
           'bg-g-400 text-b-navy hover:bg-g-400/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80',
+        third: 'bg-white border border-b-orange text-b-orange rounded-full',
         // ghost:
         //   'hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50',
         // link: 'text-slate-900 underline-offset-4 hover:underline dark:text-slate-50',
       },
       size: {
         default: 'h-10 px-4 py-2',
+        xs: 'px-3 py-1.5 text-B4_12_Regular ',
         sm: 'h-9 rounded-md px-3',
         lg: 'rounded-[1.25rem] py-3 w-[16.25rem]',
         icon: 'h-10 w-10',
