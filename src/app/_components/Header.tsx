dd1088eaf10d20e43b6cdd812d7e5ba12c9a99ef
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2025-01-11 15:51:48 +0900

    feat: modify main image size

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -10,14 +10,16 @@ export const Header = () => {
         <Image src={tebahfreeLogo} alt="tebahfree-logo" priority sizes="100vw" />
       </div>
       <div className="relative">
-        <div className="absolute left-6 top-14">
+        <div className="absolute left-6 top-14 z-10">
           <div className="text-3xl font-light text-g-white">은헤광성교회</div>
           <div className="text-3xl font-bold text-g-white">청년부</div>
           <div className="mt-4 whitespace-pre-line text-sm font-light text-g-white">
             {`은혜광성교회 청년부는 진리를 통해 자유함을 누리는\n청년들의 공동체 입니다`}
           </div>
         </div>
-        <Image src={mainImg} alt="main-img" priority sizes="100vw" />
+        <div className="relative aspect-[375/436] w-dvw min-w-[360px] max-w-[600px]">
+          <Image src={mainImg} alt="main-img" priority fill />
+        </div>
       </div>
     </header>
   )
