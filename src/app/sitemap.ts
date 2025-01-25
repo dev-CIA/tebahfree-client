8e6d5b570a5b73a933ba2d38594550ec33cf6e85
Inae Choi <inaechoi.dev@gmail.com>
2025-01-25 21:20:07 +0900

    fix: delete unnecessary function

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,10 +1,5 @@
 import type { MetadataRoute } from 'next'
 
-export async function generateSitemaps() {
-  // Fetch the total number of products and calculate the number of sitemaps needed
-  return [{ id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }]
-}
-
 export default function sitemap(): MetadataRoute.Sitemap {
   return [{ url: 'https://www.gksyouth.kr' }]
 }
